b9281e9704fcbe24730448e07e4252bfe5d014b1
João Paulo Costa <joao-paulocosta@live.com>
2020-11-25 18:47:18 -0300

    (fix) Footer proper itens alignin

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
-/* Open when someone clicks on the span element */
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-}
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-}
-
+// Initial first that will only run when page get ready
 $(document).ready(function () {
+    // Sets "Moment" libraly to Spanish 
     moment.locale('es');
+
+    // Getting current time and setting it to the mentioned div
     $('#timeDiv').text(moment().format("dddd, D [de] MMMM [de] YYYY"));
+
+    // ...
     $(window).resize(function () {
         if ($(window).width() < 850) {
             $('#menu-overlap').css("display", "none");
         } else {
             $('#menu-overlap').css("display", "inline");
         }
-    });
-
-    
-
+    }); 
 
 });
